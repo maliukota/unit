20863b323969376d42495606a05e785d1fd1a8c0
Tatiana Maliukova <skorobreshik@gmail.com>
2019-11-30 14:55:41 +0700

    Added 3 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    void shouldReturn100IfAmountIs1900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1900;
+
+        int expectedResult = 100;
+        int actualResult = cashbackHackService.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int expectedResult = 0;
+        int actualResult = cashbackHackService.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturn200IfAmountIs800() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 800;
+
+        int expectedResult = 200;
+        int actualResult = cashbackHackService.remain(amount);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
