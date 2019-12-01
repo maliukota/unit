7baad4997cfc5fa447c91355081feb4b6bd9db73
Tatiana Maliukova <skorobreshik@gmail.com>
2019-12-01 15:50:20 +0700

    Added CashbackHackData.csv and modified three tests without exceptions to Parameterized test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,59 +1,22 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturn100IfAmountIs1900() {
+    @ParameterizedTest
+    @CsvFileSource(resources= "/CashbackHackData.csv",numLinesToSkip=1)
+    void shouldCalculateRemainOfAmount(int amount, int expectedResult, String message) {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1900;
 
-        int expectedResult = 100;
         int actualResult = cashbackHackService.remain(amount);
 
-        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult, actualResult, message);
     }
-
-    @Test
-    void shouldReturn0IfAmountIs1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-
-        int expectedResult = 0;
-        int actualResult = cashbackHackService.remain(amount);
-
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    void shouldReturn200IfAmountIs800() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 800;
-
-        int expectedResult = 200;
-        int actualResult = cashbackHackService.remain(amount);
-
-        assertEquals(expectedResult, actualResult);
-    }
-
-//    @Test
-//    void shouldReturnMessageIfAmountIsLowerOrEquals0() throws IOException {
-//        CashbackHackService cashbackHackService = new CashbackHackService();
-//        int amount = 0;
-//
-//        String expectedResult = "amount must be greater than zero";
-//        int actualResult = cashbackHackService.remain(amount);
-//
-//        Throwable thrown = assertThrows(IOException.class, () -> {
-//            assertEquals(expectedResult, actualResult);
-//        });
-//                assertNotNull(thrown.getMessage());
-//    }
 
     @Test
     void shouldReturnMessageIfAmountIsLowerOrEquals0() {
